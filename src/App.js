3eaae56aae9d6cd738257a74d22975f9cde712ff
shinbian11 <shinbian11@gmail.com>
2022-06-29 13:06:07 +0900

    서버에서 데이터 fetch 해서 가져오기 (nav 컴포넌트에 적용)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Header } from "./Header";
 import { Link, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Welcome } from "./Welcome";
 
-function Nav() {
+function Nav(props) {
   return (
     <nav>
       <ol>
-        <li>
-          <Link to="/">welcome</Link>
-        </li>
-        <li>
-          <Link to="/read/1">read</Link>
-        </li>
+        {props.data.map((e) => (
+          <li key={e.id}>
+            <Link to={`/read/${e.id}`}>{e.title}</Link>
+          </li>
+        ))}
       </ol>
     </nav>
   );
@@ -26,20 +27,27 @@ function Read() {
     </article>
   );
 }
-function Welcome() {
-  return (
-    <article>
-      <h2>Welcome</h2>
-      Hello, WEB
-    </article>
-  );
-}
 
 function App() {
+  // 1. useEffect를 이용, 한번만 실행
+  // 2. fetch 를 이용
+  // 3. topics state를 갱신
+  const [topics, setTopics] = useState([]);
+
+  async function refresh() {
+    const resp = await fetch("http://localhost:3333/topics");
+    const data = await resp.json();
+    setTopics((current) => data);
+  }
+
+  useEffect(() => {
+    refresh();
+  }, []);
+
   return (
     <div>
       <Header></Header>
-      <Nav></Nav>
+      <Nav data={topics}></Nav>
       <Routes>
         <Route exact path="/" element={<Welcome />}></Route>
         <Route path="/read/1" element={<Read />}></Route>
